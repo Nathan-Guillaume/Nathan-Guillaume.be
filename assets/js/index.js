fde0e33e1b70ed54b4effd8e288b8d08ae3bd4e8
Nathan-Guillaume <45270643+Nathan-Guillaume@users.noreply.github.com>
2021-01-25 20:36:26 +0100

    Delete all

--- assets/js/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("#switch_lang").click(function () {
-        const lang_now = document.cookie.match(/lang=[a-zA-Z]+/g);
-        const lang_after = lang_now[0].toLowerCase() === 'lang=fr' ? 'en' : 'fr';
-        Cookies.set('lang', lang_after);
-        $("body").load(window.location.href);
-    });
-});
